1d7aa7ef6a2d59fbf66e12d8a9344bd1343015cf
bluewink <59544716+bluewink@users.noreply.github.com>
2020-09-22 23:32:31 +0900

    make static header and set background-color of chats

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import styled from 'styled-components';
-
+import React from "react";
+import styled from "styled-components";
+import { images } from "./images/image";
 export default function Header() {
-	return <Wrapper>발신자 변경해주는 상단바</Wrapper>;
+  return (
+    <Wrapper>
+      <ProfileImage></ProfileImage>
+      <div>
+        <h2 style={{ marginBottom: "0" }}>고은</h2>
+        현재 활동중
+      </div>
+    </Wrapper>
+  );
 }
 const Wrapper = styled.div`
-	background-color: black;
-	color: white;
+  background-color: rgba(0, 0, 0, 0.1);
+  height: 20vh;
+  color: black;
+  display: flex;
+  align-items: center;
+`;
+
+const ProfileImage = styled.div`
+  background-image: url(${images.COOL});
+  width: 100px;
+  height: 100px;
+  background-size: cover;
+  border-radius: 25px;
+  display: inline-block;
+  margin: 15px;
 `;
---
+++ src/components/images/image.js
@@ -0,0 +1,4 @@
+export const images = {
+  EUNKO: "https://img.techpowerup.org/200908/eun.png",
+  COOL: "https://img.techpowerup.org/200908/NjRiY2JjOGU5YzQz.png",
+};
