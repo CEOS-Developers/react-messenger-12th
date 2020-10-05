960056248119c5cc3ba797b3057e84d463de1799
EunKo <58415191+eun-ko@users.noreply.github.com>
2020-10-05 13:00:57 +0900

    Revert "[2주차] 서유빈 과제 제출합니다"

--- src/App.js
+++ src/App.js
@@ -4,19 +4,16 @@ import styled from 'styled-components';
 import ChattingScreen from './components/ChattingScreen';
 
 export default function App() {
-  return (
-    <Wrapper>
-      {' '}
-      {/*Wrapper components are components that surround unknown components and provide a default structure to display the child components.*/}
-      <ChattingScreen></ChattingScreen>
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<ChattingScreen></ChattingScreen>
+		</Wrapper>
+	);
 }
 
 const Wrapper = styled.div`
-  background-color: #abc7d1;
-  height: 100vh;
-  & * {
-    font-family: sans-serif;
-  }
+	height: 100vh;
+	& * {
+		font-family: sans-serif;
+	}
 `;
