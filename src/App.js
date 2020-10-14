0e843908c8e01dd16f2268ea4740807292fcf32d
Thea Seo <anatsolarborn@gmail.com>
2020-10-14 19:18:42 -0400

    implement search function

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React from 'react';
 import styled from 'styled-components';
 
 import ChattingScreen from './components/ChattingScreen';
+import InstaDMScreen from './components/InstaFriendsScreen';
+import DMListScreen from './components/DMListScreen';
 
 export default function App() {
   return (
     <Wrapper>
       {' '}
       {/*Wrapper components are components that surround unknown components and provide a default structure to display the child components.*/}
-      <ChattingScreen></ChattingScreen>
+      <DMListScreen></DMListScreen>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
-  background-color: #abc7d1;
+  
   height: 100vh;
   & * {
     font-family: sans-serif;
