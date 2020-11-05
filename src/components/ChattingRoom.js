29fff4c34198c5ad9dc3faea9317d17dc7a38b41
Hooniya-nolja <wkdckdgns01@naver.com>
2020-11-06 00:55:30 +0900

    set Username and ChatPreview with variable - Edit ChattingRoom component

--- src/components/ChattingRoom.js
+++ src/components/ChattingRoom.js
@@ -1,54 +1,46 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-export default function ChattingRoom() {
-    const eunProfile = 'https://img.techpowerup.org/200908/eun.png'
+export default function ChattingRoom({ userNum }) {
+  const eunProfile = 'https://img.techpowerup.org/200908/eun.png';
+  const nameArr = ['고은', '문상빈', '문상진', '유빈', '이재용',
+   '정쿨', '지노', '황유나', '유현우', '장창훈'];
+  const chattingPreviewArr = ['안녕하세요'];
 
-    return (
-        <Wrapper>
-            <ChatterProfile src={eunProfile} />
-            <Link to="chatting-screen">
-                <ChattingInfo>
-                    <ChatterName>
-                        고은
-                    </ChatterName>
-                    <ChattingPreview>
-                        안녕하세요...
-                    </ChattingPreview>
-                </ChattingInfo>
-            </Link>
-        </Wrapper>
-    )
+  return (
+    <Wrapper>
+      <ChatterProfile src={eunProfile} />
+      <Link to="chatting-screen">
+        <ChattingInfo>
+          <ChatterName>{nameArr[userNum]}</ChatterName>
+          <ChattingPreview>{chattingPreviewArr[0]}</ChattingPreview>
+        </ChattingInfo>
+      </Link>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
-    height: 10%;
-    width: 100%;
-    background: #ce93d8;
+  height: 10%;
+  width: 100%;
+  background: #ce93d8;
 
-    display: flex;
-`
+  display: flex;
+`;
 
 const ChatterProfile = styled.img`
-    height: 80%;
-    width: 14%;
-    border-radius: 50%;
-    align-self: center;
-`
+  height: 80%;
+  width: 14%;
+  border-radius: 50%;
+  align-self: center;
+`;
 
 const ChattingInfo = styled.div`
-    display: flex;
-    flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 
-const ChatterName = styled.div`
-`
+const ChatterName = styled.div``;
 
-const ChattingPreview = styled.div`
-`
+const ChattingPreview = styled.div``;
--- src/components/ChattingRoomList.js
+++ src/components/ChattingRoomList.js
@@ -3,27 +3,27 @@ import styled from 'styled-components';
 import ChattingRoom from './ChattingRoom';
 
 export default function ChattingRoomList() {
-    return (
-        <Wrapper>
-            <ChattingRoom />
-            <ChattingRoom />
-            <ChattingRoom />
-            <ChattingRoom />
-            <ChattingRoom />
-            <ChattingRoom />
-            <ChattingRoom />
-            <ChattingRoom />
-            <ChattingRoom />
-            <ChattingRoom />
-        </Wrapper>
-    )
+  return (
+    <Wrapper>
+      <ChattingRoom userNum={0}/>
+      <ChattingRoom userNum={1}/>
+      <ChattingRoom userNum={2}/>
+      <ChattingRoom userNum={3}/>
+      <ChattingRoom userNum={4}/>
+      <ChattingRoom userNum={5}/>
+      <ChattingRoom userNum={6}/>
+      <ChattingRoom userNum={7}/>
+      <ChattingRoom userNum={8}/>
+      <ChattingRoom userNum={9}/>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
-    height: 100%;
-    width: 100%;
-    background: grey;
-`
+  height: 100%;
+  width: 100%;
+  background: grey;
+`;
 
 // const ChattingRoom = styled.div`
 //     height: 10%;
