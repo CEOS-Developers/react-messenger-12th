484af74f5b35b59cc12dc4a2ec45b0ee490dff69
Thea Seo <anatsolarborn@gmail.com>
2020-11-06 07:05:25 -0500

    add search filter to setting

--- src/components/UserSettingScreen.js
+++ src/components/UserSettingScreen.js
@@ -3,62 +3,64 @@ import React, { useState, useEffect } from 'react';
 import { PersonAdd } from '@styled-icons/ionicons-outline/PersonAdd';
 import { Link } from 'react-router-dom';
 import { ChevronRight } from '@styled-icons/boxicons-regular/ChevronRight';
-import {Timeline} from '@styled-icons/material/Timeline';
-import {Notifications} from '@styled-icons/ionicons-outline/Notifications';
-import {PrivacyTip} from '@styled-icons/material/PrivacyTip';
-import {HelpCircle} from '@styled-icons/boxicons-regular/HelpCircle';
+import { Timeline } from '@styled-icons/material/Timeline';
+import { Notifications } from '@styled-icons/ionicons-outline/Notifications';
+import { PrivacyTip } from '@styled-icons/material/PrivacyTip';
+import { HelpCircle } from '@styled-icons/boxicons-regular/HelpCircle';
 
+const SettingFactors = [
+  { text: 'Follow and Invite Friends', icon: <PersonAdd size='20' />,},
+  { text: 'Your Activity', icon: <Timeline size='20' /> },
+  { text: '  Notifications', icon: <Notifications size='20' /> },
+  { text: 'Privacy', icon: <PrivacyTip size='20' /> },
+  { text: ' Help', icon: <HelpCircle size='20' /> },
+];
 export default function UserSettingScreen({}) {
   const [color, setColor] = useState('white');
+  const [search, setSearch] = useState('');
+  const [settingList, setSettingList] = useState(SettingFactors);
+  const [filteredSettings, setFilteredSettings] = useState([]);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    setFilteredSettings(
+      settingList.filter((setting) => {
+        return setting.text.toLowerCase().includes(search.toLowerCase());
+      })
+    );
+  }, [search, settingList]);
   return (
     <Wrapper>
-      <Search>
-        <SettingSection>
-          <Blank>
-            <PersonAdd size='20' />
-          </Blank>
-          Follow and Invite Friends
-          <ChevronRight size='25' color='grey' />
-        </SettingSection>
-        <SettingSection>
-          <Blank>
-            <Timeline size='20' />
-          </Blank>
-          Your Activity
-          <ChevronRight size='25' color='grey' />
-        </SettingSection>
-        <SettingSection>
-          <Blank>
-            <Notifications size='20' />
-          </Blank>
-          Notifications
-          <ChevronRight size='25' color='grey' />
-        </SettingSection>
-        <SettingSection>
-          <Blank>
-            <PrivacyTip size='20' />
-          </Blank>
-          Privacy
-          <ChevronRight size='25' color='grey' />
-        </SettingSection>
-        <SettingSection>
-          <Blank>
-            <HelpCircle size='20' />
-          </Blank>
-          Help
-          <ChevronRight size='25' color='grey' />
-        </SettingSection>
-     <hr class="solid"></hr>
-    <Blank> <h3 >Logins</h3></Blank>
-    <SettingSection>
-          Login info
-          <ChevronRight size='25' color='grey' />
-        </SettingSection>
-        <SettingSection>Add Account</SettingSection>
-        <SettingSection>Log Out</SettingSection>
+      <SearchBox
+        type='text'
+        placeholder='search'
+        onChange={handleChange}
+        size='50'
+      ></SearchBox>
 
-
-      </Search>
+      {filteredSettings.map((info, index) => {
+        return (
+          <SettingSection>
+            <Blank>{info.icon}</Blank>
+            <SettingText>{info.text}</SettingText>
+            <ChevronRight size='25' color='grey' />
+          </SettingSection>
+        );
+      })}
+      <hr class='solid'></hr>
+      <Blank>
+        {' '}
+        <h3>Logins</h3>
+      </Blank>
+      <SettingSection>
+        Login info
+        <ChevronRight size='25' color='grey' />
+      </SettingSection>
+      <SettingSection>Add Account</SettingSection>
+      <SettingSection>Log Out</SettingSection>
     </Wrapper>
   );
 }
@@ -66,18 +68,18 @@ export default function UserSettingScreen({}) {
 const Wrapper = styled.div``;
 const Search = styled.div`
   margin-top: 10px;
-  
 `;
-const LoginsSetting=styled.h4`
-margin-left: 15px;
-margin-top:10px;
-color:skyblue;
-font-size: 15px;
+const LoginsSetting = styled.h4`
+  margin-left: 15px;
+  margin-top: 10px;
+  color: skyblue;
+  font-size: 15px;
 `;
 const SettingSection = styled.button`
   width: 80%;
   font-size: 18px;
   margin-left: 10px;
+  margin-top: 10px;
   display: flex;
   border: none;
   background-color: white;
@@ -88,8 +90,20 @@ const SettingSection = styled.button`
     color: black;
   }
 `;
-
+const SettingText = styled.ul`
+  margin-top: 5px;
+  margin-left: -40px;
+  font-size: 15px;
+`;
 const Blank = styled.div`
   margin-right: 10px;
   margin-left: 10px;
+`;
+const SearchBox = styled.input`
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 10%;
+  margin-top: 10px;
+  margin-right: 15px;
+  margin-left: 15px;
+  font-size:20px;
 `;
